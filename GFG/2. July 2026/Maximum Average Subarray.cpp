4809ec30835a697b8f21167c87f8cc448471abd7
Gohil Vishal <gohilvd04@gmail.com>
2026-07-26 22:10:47 +0530

    Sliding Window - Fixed Length

--- GFG/2. July 2026/Maximum Average Subarray.cpp
+++ GFG/2. July 2026/Maximum Average Subarray.cpp
@@ -23,3 +23,26 @@ class Solution {
         
     }
 };
+
+// LeetCode: 643. Maximum Average Subarray I
+
+// Return the average of the subarray with the maximum average value. 
+// The function should return a double representing the maximum average of any contiguous subarray of length k.
+// class Solution {
+// public:
+//     double findMaxAverage(vector<int>& arr, int k) {
+//         int sum = 0;
+//         int n = arr.size();
+
+//         for(int i = 0; i<k; i++){
+//             sum += arr[i];
+//         }
+
+//         int maxSum = sum;
+//         for(int i =k; i<n; i++){
+//             sum += arr[i] - arr[i-k];
+//             maxSum = max(maxSum, sum);
+//         }
+//         return (double) maxSum/k;
+//     }
+// };
