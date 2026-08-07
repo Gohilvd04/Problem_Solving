f4d26ffe06e4b8e7347ad6d0cd0c45ce38f305ea
Gohil Vishal <gohilvd04@gmail.com>
2026-08-08 00:33:12 +0530

    Sliding Window - Variable Length

--- GFG/3. August 2026/424. Longest Repeating Character Replacement.cpp
+++ GFG/3. August 2026/424. Longest Repeating Character Replacement.cpp
@@ -2,9 +2,28 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int left = 0, right = 0, ans = 0, maxFreq = 0;
+        int left = 0, right = 0, ans = 0;
+        int  maxFreq = 0;
         int freq[26] = {};
 
+        // for(int i = 0; i<n; i++){
+        //     int freq[26] = {};
+        //     int  maxFreq = 0, changes = 0;
+
+        //     for(int j =i; j<n; j++){
+        //         freq[s[j] - 'A']++;
+        //         maxFreq = max(maxFreq, freq[s[j] - 'A']);
+
+        //         changes = (j-i+1) - maxFreq;
+        //         if(changes <= k){
+        //             ans = max(ans, j-i+1);
+        //         }else{
+        //             break;
+        //         }
+        //     } 
+
+        // }
+        
         while(right < n){
             freq[s[right] - 'A']++;
             maxFreq = max(maxFreq, freq[s[right] - 'A']);
